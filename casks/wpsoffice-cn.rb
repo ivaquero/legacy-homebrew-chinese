abae3b9a98f38bbdc6d6391f485f369f56911f97
ivaquero <46377665+integzz@users.noreply.github.com>
2021-03-18 19:26:50 +0800

    Update wpsoffice-cn.rb

--- casks/wpsoffice-cn.rb
+++ casks/wpsoffice-cn.rb
@@ -1,6 +1,6 @@
 cask "wpsoffice-cn" do
   version "3.3.0,5120"
-  sha256 "2c6fb103b7123c17ff00d11d3e6e00f74e1c293198fe8848415abc151a1f1f1c"
+  sha256 "b36de75645e2fab6423a9f87887ed60245583ff553708cfcad57a8e04b08685c"
 
   # package.mac.wpscdn.cn was verified as official when first introduced to the cask
   url "https://package.mac.wpscdn.cn/mac_wps_pkg/#{version.before_comma}/WPS_Office_#{version.before_comma}(#{version.after_comma}).dmg"
