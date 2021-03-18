42767cfaf62a06c268dc08521ff5b80b4c3006b1
ivaquero <46377665+integzz@users.noreply.github.com>
2021-03-18 19:10:30 +0800

    Create julia-linux.rb

---
+++ Formula/julia-linux.rb
@@ -0,0 +1,22 @@
+class JuliaBinary < Formula
+  desc "The Julia programming language"
+  homepage "https://julialang.org"
+  version "1.5.0"
+
+  option "with-mirror-bfsu", "Download from https://mirrors.bfsu.edu.cn, use this option if you're in mainland China for a faster downloading speed."
+
+    if build.with? "mirror-bfsu"
+        url "https://mirrors.bfsu.edu.cn/julia-releases/bin/linux/x64/1.5/julia-1.5.0-linux-x86_64.tar.gz"
+    else
+        url "https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-1.5.0-linux-x86_64.tar.gz"
+    end
+
+    sha256 "be7af676f8474afce098861275d28a0eb8a4ece3f83a11027e3554dcdecddb91"
+
+  bottle :unneeded
+
+  def install
+    libexec.install Dir["*"]
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+end
