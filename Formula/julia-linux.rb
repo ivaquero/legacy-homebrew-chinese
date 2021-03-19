212c7e5b5348558244f67e309d22766bb36f4aea
ivaquero <46377665+integzz@users.noreply.github.com>
2021-03-19 12:02:08 +0800

    Update julia-linux.rb

--- Formula/julia-linux.rb
+++ Formula/julia-linux.rb
@@ -6,9 +6,9 @@ class JuliaLinux < Formula
   option "with-mirror-bfsu", "Download from https://mirrors.bfsu.edu.cn, use this option if you're in mainland China for a faster downloading speed."
 
     if build.with? "mirror-bfsu"
-        url "https://mirrors.bfsu.edu.cn/julia-releases/bin/linux/x64/1.5/julia-#{version}-linux-x86_64.tar.gz"
+        url "https://mirrors.bfsu.edu.cn/julia-releases/bin/linux/x64/#{version.major_minor}/julia-#{version}-linux-x86_64.tar.gz"
     else
-        url "https://julialang-s3.julialang.org/bin/linux/x64/1.5/julia-#{version}-linux-x86_64.tar.gz"
+        url "https://julialang-s3.julialang.org/bin/linux/x64/#{version.major_minor}/julia-#{version}-linux-x86_64.tar.gz"
     end
 
     sha256 "80dec351d1a593e8ad152636971a48d0c81bfcfab92c87f3604663616f1e8bc5"
