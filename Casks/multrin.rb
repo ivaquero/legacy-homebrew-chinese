4718abcd1e13b5dc2e779f2b5f91d944852c5ecd
ivaquero <46377665+integzz@users.noreply.github.com>
2021-03-25 13:41:55 +0800

    Update multrin.rb

--- Casks/multrin.rb
+++ Casks/multrin.rb
@@ -3,7 +3,7 @@ cask "multrin" do
   sha256 "ece7c72fcb0ddaf839932032b910b990da339dc4fc7e7f2f8eac96ce79813925"
 
   url "https://github.com/sentialx/multrin/releases/download/v#{version}/Multrin-#{version}.dmg"
-  appcast "https://github.com/xiandanin/magnetW/releases.atom"
+  appcast "https://github.com/sentialx/multrin/releases.atom"
   name "Multrin"
   desc "Organize apps windows in tabs like in abandoned Windows Sets and more"
   homepage "https://github.com/sentialx/multrin/"
