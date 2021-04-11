f6ea3d7ca55e69f3eb9aca18cdc77988dc4ea548
ivaquero <46377665+integzz@users.noreply.github.com>
2021-04-11 19:59:45 +0800

    fix

---
+++ Casks/wine-latest.rb
@@ -0,0 +1,60 @@
+cask "wine-latest" do
+  version "5.7"
+  sha256 "04a52f91ea515dfdcbead6fc3efefccf4e53377d17b9e818d14b44f29f184f28"
+
+  url "https://dl.winehq.org/wine-builds/macosx/pool/winehq-staging-#{version}.pkg"
+  name "WineHQ-staging"
+  desc "Compatibility layer to run Windows applications"
+  homepage "https://wiki.winehq.org/MacOS"
+
+  livecheck do
+    url "https://dl.winehq.org/wine-builds/macosx/download.html"
+    strategy :page_match
+    regex(%r{href=.*?/winehq-staging-(\d+(?:\.\d+)*)\.pkg}i)
+  end
+
+  conflicts_with cask: [
+    "wine-devel",
+    "wine-staging",
+  ]
+  depends_on cask: "xquartz-for-wine"
+
+  pkg "winehq-staging-#{version}.pkg",
+      choices: [
+        {
+          "choiceIdentifier" => "choice3",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 1,
+        },
+      ]
+  binary "#{appdir}/Wine staging.app/Contents/Resources/start/bin/appdb"
+  binary "#{appdir}/Wine staging.app/Contents/Resources/start/bin/winehelp"
+  binary "#{appdir}/Wine staging.app/Contents/Resources/wine/bin/msiexec"
+  binary "#{appdir}/Wine staging.app/Contents/Resources/wine/bin/notepad"
+  binary "#{appdir}/Wine staging.app/Contents/Resources/wine/bin/regedit"
+  binary "#{appdir}/Wine staging.app/Contents/Resources/wine/bin/regsvr32"
+  binary "#{appdir}/Wine staging.app/Contents/Resources/wine/bin/wine"
+  binary "#{appdir}/Wine staging.app/Contents/Resources/wine/bin/wine64"
+  binary "#{appdir}/Wine staging.app/Contents/Resources/wine/bin/wineboot"
+  binary "#{appdir}/Wine staging.app/Contents/Resources/wine/bin/winecfg"
+  binary "#{appdir}/Wine staging.app/Contents/Resources/wine/bin/wineconsole"
+  binary "#{appdir}/Wine staging.app/Contents/Resources/wine/bin/winedbg"
+  binary "#{appdir}/Wine staging.app/Contents/Resources/wine/bin/winefile"
+  binary "#{appdir}/Wine staging.app/Contents/Resources/wine/bin/winemine"
+  binary "#{appdir}/Wine staging.app/Contents/Resources/wine/bin/winepath"
+  binary "#{appdir}/Wine staging.app/Contents/Resources/wine/bin/wineserver"
+
+  uninstall pkgutil: [
+    "org.winehq.wine-staging",
+    "org.winehq.wine-staging-deps",
+    "org.winehq.wine-staging-deps64",
+    "org.winehq.wine-staging32",
+    "org.winehq.wine-staging64",
+  ],
+            delete:  "/Applications/Wine staging.app"
+
+  caveats <<~EOS
+    #{token} installs support for running 64 bit applications in Wine, which is considered experimental.
+    If you do not want 64 bit support, you should download and install the #{token} package manually.
+  EOS
+end
