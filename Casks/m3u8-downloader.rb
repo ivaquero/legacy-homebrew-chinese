8d83fcf3750b9432ccf38f683466088e08388ae4
ivaquero <msaintms@outlook.com>
2022-04-19 03:28:54 +0800

    fix: 🐛 fix m3u8-downloader

--- Casks/m3u8-downloader.rb
+++ Casks/m3u8-downloader.rb
@@ -1,35 +1,38 @@
 cask "m3u8-downloader" do
-    version "2.0.7"
-  
-    if Hardware::CPU.intel?
-      sha256 "033e5383fec82327196d8a58834a50f95a37d2e4dde9f397cdd34317a2a7d8d7"
-  
-      url "https://github.com/HeiSir2014/M3U8-Downloader/releases/download/v#{version}/M3U8-Downloader-mac_#{arch}-#{version}.dmg",
-          verified: "github.com/HeiSir2014/M3U8-Downloader/"
-    else
-      sha256 "ac5ad32fe771ad09539f3dadfec65ff5b956de76316774f87b2abc6901d4c103"
-  
-      url "https://github.com/HeiSir2014/M3U8-Downloader/releases/download/v#{version}/M3U8-Downloader-mac_#{arch}-#{version}.dmg",
-          verified: "github.com/HeiSir2014/M3U8-Downloader/"
-    end
-  
-    name "M3U8-Downloader"
-    desc "Open-source M3U8 Downloader"
-    homepage "https://tools.heisir.cn/HLSDownload/"
-  
-    livecheck do
-      url :url
-      strategy :github_latest
-    end
+    
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
-    app "M3U8-Downloader.app"
+  version "2.0.7"
   
-    zap trash: [
-      "~/Library/Application Support/M3U8-Downloader",
-      "~/Library/Preferences/cn.heisir.m3u8-downloader-mac.plist",
-      "~/Library/Saved Application State/cn.heisir.m3u8-downloader-mac.savedState",
-      "~/Library/Logs/M3U8-Downloader",
-      "~/Library/Caches/cn.heisir.m3u8-downloader-mac",
-    ]
+  if Hardware::CPU.intel?
+    sha256 "033e5383fec82327196d8a58834a50f95a37d2e4dde9f397cdd34317a2a7d8d7"
+
+    url "https://github.com/HeiSir2014/M3U8-Downloader/releases/download/v#{version}/M3U8-Downloader-mac_#{arch}-#{version}.dmg",
+        verified: "github.com/HeiSir2014/M3U8-Downloader/"
+  else
+    sha256 "ac5ad32fe771ad09539f3dadfec65ff5b956de76316774f87b2abc6901d4c103"
+
+    url "https://github.com/HeiSir2014/M3U8-Downloader/releases/download/v#{version}/M3U8-Downloader-mac_#{arch}-#{version}.dmg",
+        verified: "github.com/HeiSir2014/M3U8-Downloader/"
   end
+  
+  name "M3U8-Downloader"
+  desc "Open-source M3U8 Downloader"
+  homepage "https://tools.heisir.cn/HLSDownload/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  app "M3U8-Downloader.app"
+
+  zap trash: [
+    "~/Library/Application Support/M3U8-Downloader",
+    "~/Library/Preferences/cn.heisir.m3u8-downloader-mac.plist",
+    "~/Library/Saved Application State/cn.heisir.m3u8-downloader-mac.savedState",
+    "~/Library/Logs/M3U8-Downloader",
+    "~/Library/Caches/cn.heisir.m3u8-downloader-mac",
+  ]
+end
   
