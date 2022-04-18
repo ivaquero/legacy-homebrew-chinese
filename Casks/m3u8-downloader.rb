3ea4d945ec4bef76da009f13e74e8ac2208d4d45
ivaquero <msaintms@outlook.com>
2022-04-19 02:06:51 +0800

    build: 👷 update livecheck of m3u8-downloader

--- Casks/m3u8-downloader.rb
+++ Casks/m3u8-downloader.rb
@@ -4,12 +4,12 @@ cask "m3u8-downloader" do
     if Hardware::CPU.intel?
       sha256 "033e5383fec82327196d8a58834a50f95a37d2e4dde9f397cdd34317a2a7d8d7"
   
-      url "https://github.com/HeiSir2014/M3U8-Downloader/releases/download/v#{version}/M3U8-Downloader-mac_x64-#{version}.dmg",
+      url "https://github.com/HeiSir2014/M3U8-Downloader/releases/download/v#{version}/M3U8-Downloader-mac_#{arch}-#{version}.dmg",
           verified: "github.com/HeiSir2014/M3U8-Downloader/"
     else
       sha256 "ac5ad32fe771ad09539f3dadfec65ff5b956de76316774f87b2abc6901d4c103"
   
-      url "https://github.com/HeiSir2014/M3U8-Downloader/releases/download/v#{version}/M3U8-Downloader-mac_arm64-#{version}.dmg",
+      url "https://github.com/HeiSir2014/M3U8-Downloader/releases/download/v#{version}/M3U8-Downloader-mac_#{arch}-#{version}.dmg",
           verified: "github.com/HeiSir2014/M3U8-Downloader/"
     end
   
@@ -17,6 +17,11 @@ cask "m3u8-downloader" do
     desc "Open-source M3U8 Downloader"
     homepage "https://tools.heisir.cn/HLSDownload/"
   
+    livecheck do
+      url :url
+      strategy :github_latest
+    end
+
     app "M3U8-Downloader.app"
   
     zap trash: [
--- Casks/wpsoffice-cn.rb
+++ Casks/wpsoffice-cn.rb
@@ -1,14 +1,17 @@
 cask "wpsoffice-cn" do
+
+  arch = Hardware::CPU.intel? ? "x64" : "arm64"
+
   version "4.2.0,6717"
 
   if Hardware::CPU.intel?
     sha256 "0e68e797253863fff96fc6f022657dab4914731ddaf4e8f32799be3bb6483a85"
 
-    url "https://package.mac.wpscdn.cn/mac_wps_pkg/#{version.csv.first}/WPS_Office_#{version.csv.first}(#{version.csv.second})_x64.dmg"
+    url "https://package.mac.wpscdn.cn/mac_wps_pkg/#{version.csv.first}/WPS_Office_#{version.csv.first}(#{version.csv.second})_#{arch}.dmg"
   else
     sha256 "756b2a7a966efb60373a572103a431dfa10c333c25ab89720a6d783d7df18498"
 
-    url "https://package.mac.wpscdn.cn/mac_wps_pkg/#{version.csv.first}/WPS_Office_#{version.csv.first}(#{version.csv.second})_arm64.dmg"
+    url "https://package.mac.wpscdn.cn/mac_wps_pkg/#{version.csv.first}/WPS_Office_#{version.csv.first}(#{version.csv.second})_#{arch}.dmg"
   end
 
   name "WPS Office"
