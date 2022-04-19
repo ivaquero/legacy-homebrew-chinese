3eb5b132d6c7f0269a4109262a87b35fbce7608c
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-04-19 17:46:28 +0800

    update m3u8-downloader

--- Casks/m3u8-downloader.rb
+++ Casks/m3u8-downloader.rb
@@ -1,21 +1,15 @@
 cask "m3u8-downloader" do
-    
   arch = Hardware::CPU.intel? ? "x64" : "arm64"
 
   version "2.0.7"
   
   if Hardware::CPU.intel?
     sha256 "033e5383fec82327196d8a58834a50f95a37d2e4dde9f397cdd34317a2a7d8d7"
-
-    url "https://github.com/HeiSir2014/M3U8-Downloader/releases/download/v#{version}/M3U8-Downloader-mac_#{arch}-#{version}.dmg",
-        verified: "github.com/HeiSir2014/M3U8-Downloader/"
   else
     sha256 "ac5ad32fe771ad09539f3dadfec65ff5b956de76316774f87b2abc6901d4c103"
-
-    url "https://github.com/HeiSir2014/M3U8-Downloader/releases/download/v#{version}/M3U8-Downloader-mac_#{arch}-#{version}.dmg",
-        verified: "github.com/HeiSir2014/M3U8-Downloader/"
   end
   
+  url "https://github.com/HeiSir2014/M3U8-Downloader/releases/download/v#{version}/M3U8-Downloader-mac_#{arch}-#{version}.dmg"
   name "M3U8-Downloader"
   desc "Open-source M3U8 Downloader"
   homepage "https://tools.heisir.cn/HLSDownload/"
