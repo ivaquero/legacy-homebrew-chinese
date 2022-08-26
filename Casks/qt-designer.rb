249604a29fca2086f1147190f30da0d734b439a3
ivaquero <46377665+ivaquero@users.noreply.github.com>
2022-08-26 08:47:49 +0800

    Update qt-designer.rb

--- Casks/qt-designer.rb
+++ Casks/qt-designer.rb
@@ -4,8 +4,8 @@ cask "qt-designer" do
 
   url "https://build-system.fman.io/static/public/files/Qt%20Designer.dmg"
   name "Qt Designer"
-  desc "A tool for quickly building graphical user interfaces with widgets from the Qt GUI framework."
-  homepage "https://build-system.fman.io/qt-designer-download/"
+  desc "Create Python GUIs with Qt in minutes"
+  homepage "https://build-system.fman.io/qt-designer-download"
 
   depends_on macos: ">= :sierra"
 
