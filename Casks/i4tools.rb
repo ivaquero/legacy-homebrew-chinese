b7a99b2517de554d2270ec3eaceeb25aa24520ba
ivaquero <msaintms@outlook.com>
2022-09-28 11:19:55 +0800

    update i4tools

--- Casks/i4tools.rb
+++ Casks/i4tools.rb
@@ -1,15 +1,15 @@
 cask "i4tools" do
-  version "1.13.010"
+  version "1.16.026"
   sha256 "d7e3116e478258dd946788f7a3774f8b71303383d612b2d105bf92f72cab146e"
 
-  url "https://d-updater.i4.cn/i4mac/20220418/0/i4Tools_v#{version}.dmg"
+  url "https://url.i4.cn/fqIBBbaa"
   name "i4Tools"
   name "爱思助手"
   desc "Mobile device management software"
   homepage "https://www.i4.cn/"
 
   livecheck do
-    url "https://url.i4.cn/fqIBBbaa"
+    url :url
     strategy :header_match
     regex(/i4Tools[._-]v?(\d+(?:\.\d+)+)\.dmg/i)
   end
