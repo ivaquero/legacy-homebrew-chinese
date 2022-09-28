3bf7e9f763b698142c323ec17d5887a66ddac42a
ivaquero <msaintms@outlook.com>
2022-09-28 11:20:09 +0800

    update i4tools

--- Casks/i4tools.rb
+++ Casks/i4tools.rb
@@ -1,6 +1,6 @@
 cask "i4tools" do
   version "1.16.026"
-  sha256 "d7e3116e478258dd946788f7a3774f8b71303383d612b2d105bf92f72cab146e"
+  sha256 "b91e082f6aa07b455a5d202f9f26a3f98a1d55013a2c6932a064a8a7ee7b8a93"
 
   url "https://url.i4.cn/fqIBBbaa"
   name "i4Tools"
