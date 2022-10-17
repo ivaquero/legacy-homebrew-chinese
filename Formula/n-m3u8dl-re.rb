684487d4753cb44b08fe1a48817c6256552b3f5c
ivaquero <msaintms@outlook.com>
2022-10-17 21:39:45 +0800

    add n-m3u8dl-re

---
+++ Formula/n-m3u8dl-re.rb
@@ -0,0 +1,9 @@
+class NM3u8dlRe < Formula
+    desc "Cross-Platform stream downloader for DASH/HLS"
+    homepage "https://github.com/nilaoda/N_m3u8DL-RE"
+    url "https://github.com/nilaoda/N_m3u8DL-RE/releases/download/v0.0.4-beta/N_m3u8DL-RE_Beta_osx-arm64_20221017.tar.gz"
+    sha256 "41e45542587caa27bf8180e66c72c6c77e83d00f8dcba2e952c5a9b04d382c6c"
+    license "MIT"
+    head "https://github.com/nilaoda/N_m3u8DL-RE.git", branch: "main"
+
+end
--- Formula/neteasemusic-tui.rb
+++ Formula/neteasemusic-tui.rb
@@ -1,7 +1,7 @@
 class NeteasemusicTui < Formula
   desc "Netease Cloud Music Terminal Client and Rust."
   homepage "https://github.com/betta-cyber/netease-music-tui"
-  version "0.1.4"
+  version "0.1.5"
   if OS.mac?
     url "https://github.com/betta-cyber/netease-music-tui/releases/download/v#{version}/netease-music-tui-macos.tar.gz"
     sha256 "9943f835d202bafdd8f39ee9a5bf9dced544773a8e866b97a74ea50707a4e6ad"
