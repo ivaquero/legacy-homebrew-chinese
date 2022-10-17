ca908fd54b33ce59088c7bfb983cd2dc7822a174
ivaquero <msaintms@outlook.com>
2022-10-17 21:59:46 +0800

    update n-m3u8dl-re

--- Formula/n-m3u8dl-re.rb
+++ Formula/n-m3u8dl-re.rb
@@ -5,7 +5,7 @@ class NM3u8dlRe < Formula
 
     if OS.mac?
       url "https://github.com/nilaoda/N_m3u8DL-RE/releases/download/v#{version}/N_m3u8DL-RE_Beta_osx-#{arch}_20221017.tar.gz"
-      sha256 "41e45542587caa27bf8180e66c72c6c77e83d00f8dcba2e952c5a9b04d382c6c"
+      sha256 "c44fc0bcad31c31b5de3b4bcdd73b160a6a550b49782fab8a18e0f4831b634c4"
     elsif OS.linux?
       url "https://github.com/nilaoda/N_m3u8DL-RE/releases/download/v#{version}/N_m3u8DL-RE_Beta_linux-#{arch}_20221017.tar.gz"
       sha256 "41e45542587caa27bf8180e66c72c6c77e83d00f8dcba2e952c5a9b04d382c6c"
