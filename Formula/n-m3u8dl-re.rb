dd0acd94dd0e776fdb255e855402764eab14b32a
ivaquero <msaintms@outlook.com>
2022-10-17 21:50:41 +0800

    add n-m3u8dl-re

--- Formula/n-m3u8dl-re.rb
+++ Formula/n-m3u8dl-re.rb
@@ -1,9 +1,18 @@
 class NM3u8dlRe < Formula
+    arch = Hardware::CPU.intel? ? "x64" : "arm64"
+
+    version "0.0.4-beta"
+
+    if OS.mac?
+      url "https://github.com/nilaoda/N_m3u8DL-RE/releases/download/v#{version}/N_m3u8DL-RE_Beta_osx-#{arch}_20221017.tar.gz"
+      sha256 "41e45542587caa27bf8180e66c72c6c77e83d00f8dcba2e952c5a9b04d382c6c"
+    elsif OS.linux?
+      url "https://github.com/nilaoda/N_m3u8DL-RE/releases/download/v#{version}/N_m3u8DL-RE_Beta_linux-#{arch}_20221017.tar.gz"
+      sha256 "41e45542587caa27bf8180e66c72c6c77e83d00f8dcba2e952c5a9b04d382c6c"
+    end
+
     desc "Cross-Platform stream downloader for DASH/HLS"
     homepage "https://github.com/nilaoda/N_m3u8DL-RE"
-    url "https://github.com/nilaoda/N_m3u8DL-RE/releases/download/v0.0.4-beta/N_m3u8DL-RE_Beta_osx-arm64_20221017.tar.gz"
-    sha256 "41e45542587caa27bf8180e66c72c6c77e83d00f8dcba2e952c5a9b04d382c6c"
     license "MIT"
-    head "https://github.com/nilaoda/N_m3u8DL-RE.git", branch: "main"
 
 end
