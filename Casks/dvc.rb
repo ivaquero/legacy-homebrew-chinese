28c4df16d3ca287203dfe54d5498134cfc9cb268
ivaquero <msaintms@outlook.com>
2022-11-03 19:53:28 +0800

    update dvc

--- Casks/dvc.rb
+++ Casks/dvc.rb
@@ -9,11 +9,7 @@ cask "dvc" do
 
   livecheck do
     url "https://dvc.org/"
-    strategy :page_match do |page|
-      match = page.match(//download/osx/dvc-(\d+(?:\.\d+)+)-(\d+)/i)
-      next if match.blank?
-
-      "#{match[1]},#{match[2]}"
+    regex(/href=.*?\/download\/osx\/dvc-(\d+(?:\.\d+)*)\.pkg/i)
     end
   end
 
