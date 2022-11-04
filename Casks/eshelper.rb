2f0eccf26385af28887f78618266822c2b527bc2
ivaquero <msaintms@outlook.com>
2022-11-04 17:22:27 +0800

    eshelp: update to 2022-10-04

--- Casks/eshelper.rb
+++ Casks/eshelper.rb
@@ -1,6 +1,6 @@
 cask "eshelper" do
-  version "2022-09-29"
-  sha256 "39a8b7af524344b26a949795fe3fe788322321e535a258107e4fcb970d6d66a9"
+  version "2022-10-04"
+  sha256 "f475593a63754d226c31de44d38cb3be2e3307de25806c701e8f006712c47d7d"
 
   # static.frdic.com/ was verified as official when first introduced to the cask
   url "https://static.frdic.com/pkg/ehmac.dmg??v=#{version}",
@@ -13,7 +13,7 @@ cask "eshelper" do
 
   livecheck do
     url "http://www.francochinois.com/v4/es/app/download"
-    strategy :sparkle
+    regex(/href=.*?dmg\?v=(\d+(?:-\d+)*)/i)
   end
 
   app "Eudic_es.app"
