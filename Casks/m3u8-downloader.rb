62107724ae7823344185284c75cc7a4b3f827318
ivaquero <msaintms@outlook.com>
2022-11-04 11:58:02 +0800

    style: 💄 brew style

--- Casks/m3u8-downloader.rb
+++ Casks/m3u8-downloader.rb
@@ -3,8 +3,8 @@ cask "m3u8-downloader" do
 
   version "2.0.7"
 
-  sha256 arm: "ac5ad32fe771ad09539f3dadfec65ff5b956de76316774f87b2abc6901d4c103",
-  intel: "033e5383fec82327196d8a58834a50f95a37d2e4dde9f397cdd34317a2a7d8d7"
+  sha256 arm:   "ac5ad32fe771ad09539f3dadfec65ff5b956de76316774f87b2abc6901d4c103",
+         intel: "033e5383fec82327196d8a58834a50f95a37d2e4dde9f397cdd34317a2a7d8d7"
 
   url "https://github.com/HeiSir2014/M3U8-Downloader/releases/download/v#{version}/M3U8-Downloader-mac_#{arch}-#{version}.dmg"
   name "M3U8-Downloader"
