c82c2287d533dc24caac10a79a711a9af721e339
ivaquero <msaintms@outlook.com>
2022-12-24 21:23:03 +0800

    clash-verge: fix

--- Casks/clash-verge.rb
+++ Casks/clash-verge.rb
@@ -3,7 +3,7 @@ cask "clash-verge" do
 
   version "1.2.1"
   sha256 arm:   "e86bd762f14818a68958814b1599adde3e12111f434e9b750dd80a4ae040f3ae",
-         intel: "c41ada415800a6626489fa2c6371be0a4f91b9115df2fdeaf57b4a69bd534315"
+         intel: "1289c836a4258b038e7e5c092fed366a890dabda19fb6645fde621cc71c8a603"
 
   url "https://github.com/zzzgydi/clash-verge/releases/download/v#{version}/Clash.Verge_#{version}_#{arch}.dmg"
   name "Clash for Windows"
