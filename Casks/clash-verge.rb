cd9a61b96e6fffe3eedb40379f3564a4aca73ba4
ivaquero <msaintms@outlook.com>
2022-12-24 21:14:15 +0800

    clash-verge: update 1.2.1

--- Casks/clash-verge.rb
+++ Casks/clash-verge.rb
@@ -1,8 +1,8 @@
 cask "clash-verge" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.2.0"
-  sha256 arm:   "0542ae337a356016e81a1d1d8ac3a6f474fd08ecb5e658b9bedded354874d6da",
+  version "1.2.1"
+  sha256 arm:   "e86bd762f14818a68958814b1599adde3e12111f434e9b750dd80a4ae040f3ae",
          intel: "c41ada415800a6626489fa2c6371be0a4f91b9115df2fdeaf57b4a69bd534315"
 
   url "https://github.com/zzzgydi/clash-verge/releases/download/v#{version}/Clash.Verge_#{version}_#{arch}.dmg"
