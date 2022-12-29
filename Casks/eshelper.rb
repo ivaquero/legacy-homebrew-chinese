37b7e5da84c7712a0c9d90453d90ba610e289a63
ivaquero <msaintms@outlook.com>
2022-12-29 20:07:35 +0800

    eshelper: update

--- Casks/eshelper.rb
+++ Casks/eshelper.rb
@@ -2,14 +2,13 @@ cask "eshelper" do
   version "2022-11-28"
   sha256 "4f61d99a3f56977376752750537f1c4d6fe1152c2478cd334e971f8e2cd9bcf8"
 
-  # static.frdic.com/ was verified as official when first introduced to the cask
   url "https://static.frdic.com/pkg/ehmac.dmg??v=#{version}",
       verified:   "static.frdic.com/",
       user_agent: :fake
   name "Eshelper"
   name "西语助手"
   desc "Spanish Chinese dictionary and learning tool"
-  homepage "http://www.francochinois.com/v4/es/app/download"
+  homepage "https://www.eudic.net/v4/es/app/eshelper"
 
   livecheck do
     url "http://www.francochinois.com/v4/es/app/download"
