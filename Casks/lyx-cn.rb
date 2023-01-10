9388749efb0dd1fe37563dd1c8a2b75c2d7c405b
ivaquero <msaintms@outlook.com>
2023-01-10 10:49:59 +0800

    update: julia-cn & lyx-cn

--- Casks/lyx-cn.rb
+++ Casks/lyx-cn.rb
@@ -1,6 +1,6 @@
 cask "lyx-cn" do
-  version "2.3.6.2"
-  sha256 "7eb15120428cbb45f5ad0e7e04c7cd314c87dd6657157bf70f5330e3d26811d0"
+  version "2.3.7"
+  sha256 "aaaaa005c5ec4bf574534de31cbd93ab4908dfa655da7535044185874a285c52"
 
   url "https://mirrors.bfsu.edu.cn/lyx/bin/#{version.major_minor_patch}/LyX-#{version}+qt5-12-x86_64-cocoa.dmg",
       verified: "mirrors.bfsu.edu.cn/lyx/"
