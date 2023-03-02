153ace168f3bbcdcc1471e09e722876dbaea8e1a
ivaquero <msaintms@outlook.com>
2023-03-02 15:51:13 +0800

    musicplayer: update to 0.2.0-alpha.10

--- Formula/musicplayer.rb
+++ Formula/musicplayer.rb
@@ -11,14 +11,14 @@ class Musicplayer < Formula
   arch = Hardware::CPU.arm? ? "aarch64" : "x86_64"
 
   if OS.mac?
-    url "https://github.com/tsirysndr/music-player/releases/download/v0.2.0-alpha.7/music-player_v0.2.0-alpha.7_#{arch}-apple-darwin.tar.gz"
+    url "https://github.com/tsirysndr/music-player/releases/download/v0.2.0-alpha.10/music-player_v0.2.0-alpha.10_#{arch}-apple-darwin.tar.gz"
     if Hardware::CPU.intel?
       sha256 "01ccd3f676e8f1073bfd26cdbd51dd4d9c6c6baa18942e7d73367bfff7119126"
     else
-      sha256 "e78ea69bed947ac8fb66fffa713d036315ae1b086f3bcd35425c829e9f9e08dc"
+      sha256 "6baaea9b96e2330771b8acc67fc9ee0e2f22263099a31ca6cded00547ee94fb0"
     end
   else
-    url "https://github.com/tsirysndr/music-player/releases/download/v0.2.0-alpha.7/music-player_v0.2.0-alpha.7_x86_64-unknown-linux-gnu.tar.gz"
+    url "https://github.com/tsirysndr/music-player/releases/download/v0.2.0-alpha.10/music-player_v0.2.0-alpha.10_x86_64-unknown-linux-gnu.tar.gz"
     sha256 "63377997ab8af0cb8c2b60b59b42c2c5539fe08cb9f985325fc058730da509dc"
   end
 
