4edc4c28058121148128ed33bfc2853f2d3a30d7
ivaquero <msaintms@outlook.com>
2023-03-02 10:37:16 +0800

    revert: ⏪️ peazip: revert to 9.0.0

--- Casks/peazip.rb
+++ Casks/peazip.rb
@@ -1,8 +1,8 @@
 cask "peazip" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "9.1.0"
-  sha256 arm:   "6bdb9c6f93f8b895cc810ddd4d77c7292f2e61364908fe1c4227917fd5a551f5",
+  version "9.0.0"
+  sha256 arm:   "556db5cc95a229dbb16c9c29085e1978fbae2138c0ddb7f935a572047bd855e8",
          intel: "65f184f2a79be59096231b9f18daf4a22018c4f47f99c4b503278b86bb60af9a"
 
   url "https://github.com/peazip/PeaZip/releases/download/#{version}/peazip-#{version}.DARWIN.#{arch}.dmg"
@@ -18,6 +18,13 @@ cask "peazip" do
   auto_updates true
 
   app "PeaZip.app"
+
+  postflight do
+    system_command "/usr/bin/open", args: ["#{staged_path}/macOS service menus/PeaZip, add to archive.workflow"]
+    system_command "/usr/bin/open", args: ["#{staged_path}/macOS service menus/PeaZip, extract here (smart new folder).workflow"]
+    system_command "/usr/bin/open", args: ["#{staged_path}/macOS service menus/PeaZip, extract....workflow"]
+    system_command "/usr/bin/open", args: ["#{staged_path}/macOS service menus/PeaZip, open file or folder.workflow"]
+  end
 
   zap trash: [
     "~/Library/Saved Application State/com.company.peazip.savedState",
