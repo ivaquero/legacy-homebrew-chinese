cc8ff55ae4ef20daa383e3762b3bea15f7824a4f
ivaquero <msaintms@outlook.com>
2023-03-11 12:57:17 +0800

    openai-translator: fix style

--- Casks/openai-translator.rb
+++ Casks/openai-translator.rb
@@ -3,7 +3,7 @@ cask "openai-translator" do
   sha256 "40616fdd8c05eabea051b03de053fd20874cee700ff8466f6e6cd796057d89c1"
 
   url "https://github.com/yetone/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_x64.dmg"
-  url "https://github.com/yetone/openai-translator/releases/download/v#{version}/OpenAI-Translator-#{version}-#{arch}-mac.zip"
+
   name "OpenAI Translator"
   desc "Cross-platform Translatior based on ChatGPT API"
   homepage "https://github.com/yetone/openai-translator"
