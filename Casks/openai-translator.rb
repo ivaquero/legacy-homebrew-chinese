479b11c0cfba15299ca2621be83cbd2de71f4269
ivaquero <msaintms@outlook.com>
2023-03-15 11:51:50 +0800

    openai-translator: update 0.0.23

--- Casks/openai-translator.rb
+++ Casks/openai-translator.rb
@@ -1,8 +1,11 @@
 cask "openai-translator" do
-  version "0.0.19"
-  sha256 "bfc6130788bb4d03a8ab6a0b24419437e7378eb63c5840341d7322de77709c52"
+  arch arm: "aarch64", intel: "universal"
 
-  url "https://github.com/yetone/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_universal.dmg"
+  version "0.0.23"
+  sha256 arm:   "1dccf520d807e2eb6510e5c0f33f8d2dd11381143ea4b1ef5c0230d371c6dcd4",
+         intel: "033e5383fec82327196d8a58834a50f95a37d2e4dde9f397cdd34317a2a7d8d7"
+
+  url "https://github.com/yetone/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_#{arch}.dmg"
 
   name "OpenAI Translator"
   desc "Cross-platform Translatior based on ChatGPT API"
