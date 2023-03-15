19b002593fbe9a4c8938ec954574fd7b33cd4087
ivaquero <msaintms@outlook.com>
2023-03-15 11:58:40 +0800

    openai-translator: update ci

--- Casks/openai-translator.rb
+++ Casks/openai-translator.rb
@@ -3,7 +3,7 @@ cask "openai-translator" do
 
   version "0.0.23"
   sha256 arm:   "1dccf520d807e2eb6510e5c0f33f8d2dd11381143ea4b1ef5c0230d371c6dcd4",
-         intel: "033e5383fec82327196d8a58834a50f95a37d2e4dde9f397cdd34317a2a7d8d7"
+         intel: "98d0cbd16797492af3b7e072870d4471a2fae7a915fd51fed76d4ead940fba93"
 
   url "https://github.com/yetone/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_#{arch}.dmg"
 
