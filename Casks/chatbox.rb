8dabe0e573d400cd40217b045c25bdea714ccb24
ivaquero <msaintms@outlook.com>
2023-04-17 21:04:07 +0800

    chatbox: update to 0.4.0

--- Casks/chatbox.rb
+++ Casks/chatbox.rb
@@ -1,8 +1,8 @@
 cask "chatbox" do
-  arch arm: "x64", intel: "x64"
+  arch arm: "aarch64", intel: "x64"
 
-  version "0.2.9"
-  sha256 arm:   "96688ba9f43e79e2fe3badf5e5bc1be472569dc9e22c2d416267a42e363c9494",
+  version "0.4.0"
+  sha256 arm:   "48297a761719702187133488197d4b39953f8e45db1811296e232e579c967af2",
          intel: "96688ba9f43e79e2fe3badf5e5bc1be472569dc9e22c2d416267a42e363c9494"
 
   url "https://github.com/Bin-Huang/chatbox/releases/download/Chatbox-v#{version}/chatbox_#{version}_#{arch}.dmg"
