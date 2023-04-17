cb11bc755eee2300a5c1846699e03e2063ffd963
ivaquero <msaintms@outlook.com>
2023-04-17 21:05:33 +0800

    chatbox: fix url

--- Casks/chatbox.rb
+++ Casks/chatbox.rb
@@ -2,8 +2,8 @@ cask "chatbox" do
   arch arm: "aarch64", intel: "x64"
 
   version "0.4.0"
-  sha256 arm:   "48297a761719702187133488197d4b39953f8e45db1811296e232e579c967af2",
-         intel: "96688ba9f43e79e2fe3badf5e5bc1be472569dc9e22c2d416267a42e363c9494"
+  sha256 arm:   "a38e8aa1d5f2fc2989b6d04083d54a6c3a6eb69b9bb9729c35d46977ea550cfc",
+         intel: "48297a761719702187133488197d4b39953f8e45db1811296e232e579c967af2"
 
   url "https://github.com/Bin-Huang/chatbox/releases/download/Chatbox-v#{version}/chatbox_#{version}_#{arch}.dmg"
   name "Chatbox"
