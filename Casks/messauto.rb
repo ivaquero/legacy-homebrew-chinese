409bf4e2f818ce5ca352ad3f647379e6d3dbe030
ivaquero <msaintms@outlook.com>
2023-06-01 16:38:44 +0800

    fix some casks

--- Casks/messauto.rb
+++ Casks/messauto.rb
@@ -1,24 +1,23 @@
 cask "messauto" do
-    arch arm: "arm64", intel: "x86_64"
+  arch arm: "arm64", intel: "x86_64"
 
-    version "0.2.2"
-    sha256 arm:   "0cb0641d0c6e2bcd0f947a50aa9fdee5439d7c544bb5a82912fd1d94b930deac",
-           intel: "57440eba0fd8f174276acf0603088f2c987d5134dacc761a38fd3b38bc6a81b7"
+  version "0.2.2"
+  sha256 arm:   "0cb0641d0c6e2bcd0f947a50aa9fdee5439d7c544bb5a82912fd1d94b930deac",
+         intel: "57440eba0fd8f174276acf0603088f2c987d5134dacc761a38fd3b38bc6a81b7"
 
-    url "https://github.com/LeeeSe/MessAuto/releases/download/v#{version}/MessAuto_#{arch}.zip"
-    name "MessAuto"
-    desc "MessAuto is a macOS app that automatically extracts and pastes and enters SMS verification codes. It is 100% developed with Rust and is suitable for any app. It is a free alternative to 2FHey."
-    homepage "https://github.com/LeeeSe/MessAuto"
-  
-    livecheck do
-        url :url
-        strategy :github_latest
-    end
-  
-    app "MessAuto.app"
-  
-    zap trash: [
-      "~/Library/Preferences/com.doe.messauto.plist",
-    ]
+  url "https://github.com/LeeeSe/MessAuto/releases/download/v#{version}/MessAuto_#{arch}.zip"
+  name "MessAuto"
+  desc "MacOS app that automatically extracts and pastes and enters SMS verification codes."
+  homepage "https://github.com/LeeeSe/MessAuto"
+
+  livecheck do
+    url :url
+    strategy :github_latest
   end
-  
+
+  app "MessAuto.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.doe.messauto.plist",
+  ]
+end
