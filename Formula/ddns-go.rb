0c98da65bc92ec7a1660781270515e7f67bf6da9
WaterLemons2k <62788816+WaterLemons2k@users.noreply.github.com>
2023-06-14 14:40:46 +0000

    ddns-go 5.3.5

--- Formula/ddns-go.rb
+++ Formula/ddns-go.rb
@@ -1,7 +1,7 @@
 class DdnsGo < Formula
   desc "Simple and easy-to-use DDNS"
   homepage "https://github.com/jeessy2/ddns-go"
-  version "5.3.4"
+  version "5.3.5"
   license "MIT"
   version_scheme 1
 
@@ -15,8 +15,8 @@ class DdnsGo < Formula
   if OS.mac?
     url "https://github.com/jeessy2/ddns-go/releases/download/v#{version}/ddns-go_#{version}_darwin_#{arch}.tar.gz"
   else
-    url "https://github.com/jeessy2/ddns-go/releases/download/v5.3.4/ddns-go_5.3.4_linux_x86_64.tar.gz"
-    sha256 "f8b626bdbff4cffcc2db1627ff5f9f6709afb1a599efa0b27f5f5b8d5d14095d"
+    url "https://github.com/jeessy2/ddns-go/releases/download/v5.3.5/ddns-go_5.3.5_linux_x86_64.tar.gz"
+    sha256 "c6ae2321bd7ee59e63aa26b827e17ded2f499aa3ca8fa1c69ed6aa2260ba4a8b"
   end
 
   def install
