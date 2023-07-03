ac8f21c7013b582ae0386ea2abd51d088befb155
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2023-07-03 15:03:34 +0800

    update gopeed.rb

---
+++ Casks/gopeed.rb
@@ -0,0 +1,22 @@
+cask "gopeed" do
+  version "1.3.3"
+  sha256 "abd0083788ebad8ae097d92ee5b35504599457d37dfcbe5ba94d192d9169b8dd"
+
+  url "https://github.com/GopeedLab/gopeed/releases/download/v#{version}/Gopeed-v#{version}-macos.dmg"
+  name "Gopeed"
+  desc "High speed downloader that supports all platforms."
+  homepage "https://gopeed.com/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  app "Gopeed.app"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.gopeed",
+    "~/Library/Containers/com.gopeed",
+  ]
+end
