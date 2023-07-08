6afb8c9cfd505eedb2dff7b046e7d7de3ad60014
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2023-07-08 09:52:09 +0800

    update shurl 1.1.0

--- Formula/shurl.rb
+++ Formula/shurl.rb
@@ -1,7 +1,7 @@
 class Shurl < Formula
   desc "A SELF-built SHORT CHAIN SHORTCUT BASED ON YOURLS"
   homepage "https://github.com/Mrered/yourlsh"
-  version "1.0.0"
+  version "1.1.0"
   license "MIT"
   version_scheme 1
 
@@ -11,7 +11,7 @@ class Shurl < Formula
   end
 
   url "https://github.com/Mrered/yourlsh/releases/download/v#{version}/shurl.tar.gz"
-  sha256 "bfc60c36db29e5d84e11e8f3b80d77a86836a76219ffe69011bbec8e821b3abd"
+  sha256 "d91647fa1de76329d49c5f99b69b3e3e145e01ccc7d634043f3da5114abbe729"
 
   def install
     bin.install "shurl"
