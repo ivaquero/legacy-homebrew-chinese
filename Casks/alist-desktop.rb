93f8cdeaa78b6d35917cfacb9b32b73cd87689f0
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2023-07-22 08:33:13 +0800

    update alist-desktop.rb 3.23.0

--- Casks/alist-desktop.rb
+++ Casks/alist-desktop.rb
@@ -1,9 +1,9 @@
 cask "alist-desktop" do
   arch arm: "aarch64", intel: "x64"
 
-  version "3.22.1"
-  sha256 arm:   "8412c7b23929e95253539c4d264e73864db3727d84e7e1c016868b5f8d7e8f62",
-         intel: "69e8f1affbbd479847a0d169e23ed4be8051d1627e0067dd9fc1711f72401a6a"
+  version "3.23.0"
+  sha256 arm:   "71eb56ef20085d2746f9d5e1936c5159d5827325b64ea1c12be1d56ff204998d",
+         intel: "a9a9fb99f66d3d1c59ff4e2bf96c7a276eb5be57bfbc401cf9c581718a8da580"
 
   url "https://github.com/alist-org/desktop-release/releases/download/v#{version}/alist-desktop_#{version}_#{arch}.dmg",
       verified:   "github.com/alist-org/"
