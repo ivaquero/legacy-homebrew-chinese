f3e4532c8e195bec65ed05099949ebf1f81e9ae8
ivaquero <msaintms@outlook.com>
2023-07-23 20:17:21 +0800

    clash-verge: update to 1.3.5

--- Casks/clash-verge.rb
+++ Casks/clash-verge.rb
@@ -1,8 +1,8 @@
 cask "clash-verge" do
   arch arm: "aarch64", intel: "x64"
 
-  version "1.3.3"
-  sha256 arm:   "188b5668052ce170f297e9fc83240f78ee4f745d8cdbccc767f01844c72a5509",
+  version "1.3.5"
+  sha256 arm:   "eff7cbf3551d26e92216ff52c5d0845c9a52b4eea54bcd587e22879a78360049",
          intel: "5930a7aa4ac2bf3341f494c81d703b242d38d6b2802177b74f2ecfb585640c87"
 
   url "https://github.com/zzzgydi/clash-verge/releases/download/v#{version}/Clash.Verge_#{version}_#{arch}.dmg"
