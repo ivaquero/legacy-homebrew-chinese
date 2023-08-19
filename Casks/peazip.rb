54a376543f80799fe02a083960d8e894005dfc1f
ivaquero <msaintms@outlook.com>
2023-08-19 17:41:47 +0800

    peazip: update to 9.4.0

--- Casks/peazip.rb
+++ Casks/peazip.rb
@@ -1,8 +1,8 @@
 cask "peazip" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "9.0.0"
-  sha256 arm:   "556db5cc95a229dbb16c9c29085e1978fbae2138c0ddb7f935a572047bd855e8",
+  version "9.4.0"
+  sha256 arm:   "9f1c361273fd2beda788a6b83a76186940bcbb9c2292bc9e1865603b9b50a28c",
          intel: "65f184f2a79be59096231b9f18daf4a22018c4f47f99c4b503278b86bb60af9a"
 
   url "https://github.com/peazip/PeaZip/releases/download/#{version}/peazip-#{version}.DARWIN.#{arch}.dmg"
