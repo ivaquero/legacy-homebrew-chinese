5eff9c13309d890d1aeb33ef071e99396fc1414e
ivaquero <msaintms@outlook.com>
2023-09-09 17:20:50 +0800

    openai-translator: update 0.0.82

--- Casks/openai-translator.rb
+++ Casks/openai-translator.rb
@@ -1,8 +1,8 @@
 cask "openai-translator" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.0.57"
-  sha256 arm:   "595bdc6dc0ed389f652c1e17e7d143e7189c317ad6334dc6365c682266012bda",
+  version "0.0.82"
+  sha256 arm:   "577f77e66fb97518e582604f1716c1f4032bf74a9b945c9d9ab30283a1a156d4",
          intel: "c2be9a24d4edd02c7cd38793c7c0d6428c025e56da8810d57205802e9f5013d1"
 
   url "https://github.com/yetone/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_#{arch}.dmg"
