28cce26d25f07a0fe5757088cbf12811d80d6458
ivaquero <msaintms@outlook.com>
2023-10-17 22:26:33 +0800

    lingquan: add 0.6.3

--- Casks/lingquan.rb
+++ Casks/lingquan.rb
@@ -1,20 +1,17 @@
 cask "lingquan" do
+  arch arm: "-arm64", intel: ""
+  folder = on_arch_conditional arm: "", intel: "/x64"
+
   version "0.6.3"
   sha256 :no_check
 
-  if Hardware::CPU.arm?
-    url "https://updater-1305474371.cos.ap-guangzhou.myqcloud.com/download/%E9%9B%B6%E6%B3%89-#{version}-arm64.dmg",
-        verified:   "updater-1305474371.cos.ap-guangzhou.myqcloud.com"
-  else
-    url "https://updater-1305474371.cos.ap-guangzhou.myqcloud.com/download/x64/%E9%9B%B6%E6%B3%89-#{version}.dmg",
-    verified:   "updater-1305474371.cos.ap-guangzhou.myqcloud.com"
-  end
-
+  url "https://updater-1305474371.cos.ap-guangzhou.myqcloud.com/download#{folder}/%E9%9B%B6%E6%B3%89-#{version}#{arch}.dmg"
   name "零泉"
-  desc "零泉可以轻松管理“图片、视频、音频、字体、3D以及设计类源文件”等，各种文件，让创意工作更加简单，高效"
+  desc "零泉可以轻松管理“图片、视频、音频、字体、3D 以及设计类源文件等，各种文件，让创意工作更加简单，高效"
   homepage "https://lingquan.cool/"
 
   auto_updates true
+
   app "零泉.app"
 
   zap trash: [
