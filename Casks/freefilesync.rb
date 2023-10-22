3b2e1b6af5107b37faa1be2517834025dde8690b
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2023-10-22 20:13:03 +0800

    update freefilesync.rb 13.0

--- Casks/freefilesync.rb
+++ Casks/freefilesync.rb
@@ -1,6 +1,6 @@
 cask "freefilesync" do
   version "13.0"
-  sha256 "2d8c529e4b62ea2c436fdc167fb43dc9c9c0bf0cc5371678dc4272ce0bfdfdd8"
+  sha256 :no_check
 
   url "https://www.freefilesync.org/download/FreeFileSync_#{version}_macOS.zip"
   name "FreeFileSync"
