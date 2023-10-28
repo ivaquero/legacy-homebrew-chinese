00f28e2319bd11bc2c441c2b2a9464ecce602605
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2023-10-28 22:55:27 +0800

    later 1.91

--- Casks/later.rb
+++ Casks/later.rb
@@ -1,5 +1,5 @@
 cask "later" do
-  version "1.0"
+  version "1.91"
   sha256 :no_check
 
   url "https://github.com/alyssaxuu/later/raw/master/Later.dmg",
@@ -9,7 +9,7 @@ cask "later" do
   homepage "https://getlater.app/"
 
   app "Later.app"
-  
+
   zap trash: [
     "~/Library/Preferences/alyssaxuu.Later.plist",
     "~/Library/Saved Application State/alyssaxuu.Later.savedState",
