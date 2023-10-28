09828124c46bc396bd95966f318c0d7625da23f5
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2023-10-28 20:58:31 +0800

    later 1.0

--- Casks/later.rb
+++ Casks/later.rb
@@ -2,7 +2,7 @@ cask "later" do
   version "1.0"
   sha256 :no_check
 
-  url "https://github.com/alyssaxuu/later/raw/master/Later.dmg"
+  url "https://github.com/alyssaxuu/later/raw/master/Later.dmg",
       verified: "github.com/alyssaxuu/later/"
   name "Later"
   desc "A simple Mac app to clear and restore your workspace with ease"
