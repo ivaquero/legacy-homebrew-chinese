97a3c1ed8dbf2f10dbee0b5acf0774a0a7aac175
Mrered Cio <34948506+Mrered@users.noreply.github.com>
2023-10-28 20:53:35 +0800

    creat: later 1.0

---
+++ Casks/later.rb
@@ -0,0 +1,12 @@
+cask "later" do
+  version "1.0"
+  sha256 :no_check
+
+  url "https://github.com/alyssaxuu/later/raw/master/Later.dmg"
+      verified: "github.com/alyssaxuu/later/"
+  name "Later"
+  desc "A simple Mac app to clear and restore your workspace with ease"
+  homepage "https://getlater.app/"
+
+  app "Later.app"
+end
