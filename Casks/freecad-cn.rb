5ccf13b34ade7ae8933c9824e3a6d75642986f73
ivaquero <msaintms@outlook.com>
2023-11-02 00:34:45 +0800

    update livecheck

--- Casks/freecad-cn.rb
+++ Casks/freecad-cn.rb
@@ -1,4 +1,6 @@
 cask "freecad-cn" do
+  arch arm: "arm64", intel: "intel_x86"
+
   version "0.21.1"
   sha256 arm:   "b6959ca9e0e2f7cddda2cf1e97a26f3e2e65205f2e8e53b3c5dccd062f0be14f",
          intel: "633dd754e7732c531a019fe74068e43883f8f329c25fe85bfcc91fa26186451f"
