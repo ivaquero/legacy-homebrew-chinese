0dc79e3069397d3cf95babced5879a3795dd08f6
ivaquero <msaintms@outlook.com>
2023-11-01 23:48:01 +0800

    openai-translator 0.0.89

--- Casks/openai-translator.rb
+++ Casks/openai-translator.rb
@@ -1,12 +1,11 @@
 cask "openai-translator" do
   arch arm: "aarch64", intel: "x64"
 
-  version "0.0.82"
-  sha256 arm:   "577f77e66fb97518e582604f1716c1f4032bf74a9b945c9d9ab30283a1a156d4",
-         intel: "c2be9a24d4edd02c7cd38793c7c0d6428c025e56da8810d57205802e9f5013d1"
+  version "0.0.89"
+  sha256 arm:   "0c6f2d261baaf8b2472465d6bd2fbf1535881cdf9c5facd7ef1e59b3c6bcc5e0",
+         intel: "e9d9f930f7837de810888e6869a80f7022a43826a6f9a4dbbc48a38e4855375f"
 
   url "https://github.com/yetone/openai-translator/releases/download/v#{version}/OpenAI.Translator_#{version}_#{arch}.dmg"
-
   name "OpenAI Translator"
   desc "Cross-platform Translatior based on ChatGPT API"
   homepage "https://github.com/yetone/openai-translator"
@@ -17,6 +16,7 @@ cask "openai-translator" do
   end
 
   auto_updates true
+
   app "OpenAI Translator.app"
 
   zap trash: [
