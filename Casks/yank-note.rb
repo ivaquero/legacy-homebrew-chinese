c4f2d431593a58948ae8da3d9def37e2fd959a83
ivaquero <msaintms@outlook.com>
2023-11-02 00:14:07 +0800

    yank-note 3.63.2

--- Casks/yank-note.rb
+++ Casks/yank-note.rb
@@ -1,12 +1,12 @@
 cask "yank-note" do
   arch arm: "arm64", intel: "x64"
 
-  version "3.60.1"
-  sha256 arm:   "9520721f5dc9cffa990d7bf82a7d790829907b253a6bddc147e90a1af0de09a9",
-         intel: "fe7928a383894e9d2a911147c0743eca92fd99b75914e01d419d7d5d8dc5a1da"
+  version "3.63.2"
+  sha256 arm:   "fd58db618bbd58e8ea0cc02613e2c4bb394e346056c357fd4f51c0b055a4b8fc",
+         intel: "bfaefe6d01df3e2ba273031d5e19b36be43b7453014c215b0e2c2c820279f411"
 
   url "https://github.com/purocean/yn/releases/download/v#{version}/Yank-Note-mac-#{arch}-#{version}.dmg",
-      verified:   "github.com/purocean/yn/"
+      verified: "github.com/purocean/yn/"
   name "Yank Note"
   desc "Highly extensible Markdown editor"
   homepage "https://yank-note.com/zh-CN"
